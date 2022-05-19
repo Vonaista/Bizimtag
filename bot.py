fef121c21aa4aaba712ef8fb76f39e65386e8b0b
Vonaista <86628538+Vonaista@users.noreply.github.com>
2022-05-19 04:56:59 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -165,7 +165,7 @@ async def mentionall(event):
     usrtxt = ""
     async for usr in client.iter_participants(event.chat_id):
       usrnum += 1
-      usrtxt += f"❤️ - [{usr.first_name}](tg://user?id={usr.id}) \n"
+      usrtxt += f"🇹🇷 - [{usr.first_name}](tg://user?id={usr.id}) \n"
       if event.chat_id not in anlik_calisan:
         await event.respond("Işlem Başarıyla Durduruldu\n\n**DESTEK KANALIMIZ @RapidDestek**❌")
         return
@@ -183,7 +183,7 @@ async def mentionall(event):
     usrtxt = ""
     async for usr in client.iter_participants(event.chat_id):
       usrnum += 1
-      usrtxt += f"❤️ - [{usr.first_name}](tg://user?id={usr.id}) \n"
+      usrtxt += f"🇹🇷 - [{usr.first_name}](tg://user?id={usr.id}) \n"
       if event.chat_id not in anlik_calisan:
         await event.respond("işlem başarıyla durduruldu❌")
         return
